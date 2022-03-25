badaa3798f44128deaf062fdb6672975b8deae09
rockydey <coderrocky25@gmail.com>
2022-03-26 03:23:54 +0600

    trash button click handle complete

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -6,9 +6,11 @@ import Cart from '../Cart/Cart';
 
 const Cars = () => {
     const [cars, setCars] = useState([]);
+
     const [vehicles, setVehicles] = useState([]);
+
     const handleAddToCart = car => {
-        if (vehicles.length < 5) {
+        if (vehicles.length < 4) {
             let newVehicle = [];
             const exits = vehicles.find(vehicle => vehicle.id === car.id);
             if (!exits) {
@@ -21,24 +23,35 @@ const Cars = () => {
             setVehicles(newVehicle);
         }
         else {
-            alert("Maximum Five Cars");
+            alert("Maximum Four Cars");
         }
-    }
+    };
+
     const pickOne = () => {
-        const picker = Math.floor(Math.random() * 5);
+        const picker = Math.floor(Math.random() * 4);
         let pickValue = [];
         pickValue.push(vehicles[picker]);
         setVehicles(pickValue);
     };
+
     const chooseAgain = () => {
         let empty = [];
         setVehicles(empty);
     };
+
+    const handleTrash = motorCar => {
+        let restCar = [];
+        const rest = vehicles.filter(vehicle => vehicle.id !== motorCar.id);
+        restCar = [...rest];
+        setVehicles(restCar);
+    };
+
     useEffect(() => {
         fetch('cars.json')
             .then(res => res.json())
             .then(data => setCars(data));
     }, []);
+
     return (
         <div className='cars'>
             <div className="cars-container mb-5">
@@ -49,7 +62,7 @@ const Cars = () => {
             <div className="cart-container rounded-3">
                 <h3 className='text-center my-3 fs-2 fw-bold'>Selected Cars</h3>
                 {
-                    vehicles.map(vehicle => <Cart key={vehicle.id} vehicle={vehicle}></Cart>)
+                    vehicles.map(vehicle => <Cart key={vehicle.id} handleTrash={handleTrash} vehicle={vehicle}></Cart>)
                 }
                 <button onClick={() => pickOne()} className='d-block px-4 py-2 border-0 rounded-3 mx-3 bg-success text-white fw-bold my-3'>Pick One</button>
                 <button onClick={() => chooseAgain()} className='d-block px-4 py-2 border-0 rounded-3 mx-3 bg-success text-white fw-bold'>Choose Again</button>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,13 +4,13 @@ import './Cart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Cart = ({ vehicle }) => {
+const Cart = ({ vehicle, handleTrash }) => {
     const { name, picture } = vehicle;
     return (
         <div className='cart d-flex justify-content-between align-items-center mx-3'>
             <img className='rounded-pill' src={picture} alt="" />
             <h4 className='fs-5 my-3'>{name}</h4>
-            <FontAwesomeIcon className='trash' icon={faTrash}></FontAwesomeIcon>
+            <FontAwesomeIcon className='trash' onClick={() => handleTrash(vehicle)} icon={faTrash}></FontAwesomeIcon>
         </div>
     );
 };
