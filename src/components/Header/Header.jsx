c5ebf23d1ebe2339df345b2f6a412e4ccc97254b
rockydey <coderrocky25@gmail.com>
2022-03-26 00:28:08 +0600

    site title, heading and json file updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <h1 className='fs-1 fw-bold mb-2 text-center'>Welcome To <span className='text-success'>CarDehko</span></h1>
+            <h1 className='fs-1 fw-bold mb-2 text-center'>Welcome To <span className='text-success'>Car Valley</span></h1>
             <p className='fs-5 fw-bold mb-5 text-center'>Presenting Premium and Wonderful Cars</p>
         </div>
     );
